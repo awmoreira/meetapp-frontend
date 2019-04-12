371147029b6fd6bfe751d1b368439aedb68ee704
Allan Winckler <awmoreira@gmail.com>
2019-04-11 23:41:49 -0300

    Component Search which will still be updated

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -14,8 +14,9 @@ import ArrowRight from '../../assets/right-arrow.svg';
 
 class Search extends Component {
   static propTypes = {
-    getMeetupsRequest: PropTypes.func.isRequired,
-    getUserRequest: PropTypes.func.isRequired,
+    getSubscriptionsRequest: PropTypes.func.isRequired,
+    getNextsRequest: PropTypes.func.isRequired,
+    getRecommendedRequest: PropTypes.func.isRequired,
     searchMeetupsRequest: PropTypes.func.isRequired,
     meetups: PropTypes.shape({
       data: PropTypes.arrayOf(
@@ -26,33 +27,36 @@ class Search extends Component {
         }),
       ),
     }).isRequired,
-    auth: PropTypes.shape({
-      user: PropTypes.shape({
-        id: PropTypes.number,
-      }),
-    }).isRequired,
   };
 
   componentDidMount() {
-    const { getMeetupsRequest, getUserRequest } = this.props;
+    const { getSubscriptionsRequest, getNextsRequest, getRecommendedRequest } = this.props;
 
-    getUserRequest();
-    getMeetupsRequest();
+    getSubscriptionsRequest();
+    getNextsRequest();
+    getRecommendedRequest();
 
     this.searchInput.focus();
   }
 
   handleInputSearch = (e) => {
-    const { searchMeetupsRequest, getMeetupsRequest } = this.props;
+    const {
+      searchMeetupsRequest,
+      getSubscriptionsRequest,
+      getNextsRequest,
+      getRecommendedRequest,
+    } = this.props;
     if (e.target.value !== '') {
       searchMeetupsRequest(e.target.value);
     } else {
-      getMeetupsRequest();
+      getSubscriptionsRequest();
+      getNextsRequest();
+      getRecommendedRequest();
     }
   };
 
   render() {
-    const { meetups } = this.props;
+    const { subscriptions, nexts, recommended } = this.props;
 
     return (
       <Container>
@@ -70,16 +74,16 @@ class Search extends Component {
 
           <span>Inscrições</span>
           <MeetupsList>
-            {meetups
-              && meetups.data.map(meetup => (
+            {subscriptions
+              && subscriptions.map(meetup => (
                 <Meetup key={meetup.id}>
                   <img alt={meetup.title} src={`http://127.0.0.1:3333/files/${meetup.file_id}`} />
                   <div>
                     <div>
                       <strong>{meetup.title}</strong>
                       <span>
-                        {meetup.subscriptions
-                          ? `${meetup.subscriptions.length} membros`
+                        {meetup.__meta__.subscriptions_count > 0
+                          ? `${meetup.__meta__.subscriptions_count} membros`
                           : 'Nenhum membro'}
                       </span>
                     </div>
@@ -93,18 +97,18 @@ class Search extends Component {
         </Box>
 
         <Box>
-          <span>Próximos meetups (30 dias)</span>
+          <span>Próximos meetups</span>
           <MeetupsList>
-            {meetups
-              && meetups.data.map(meetup => (
+            {nexts
+              && nexts.map(meetup => (
                 <Meetup key={meetup.id}>
                   <img alt={meetup.title} src={`http://127.0.0.1:3333/files/${meetup.file_id}`} />
                   <div>
                     <div>
                       <strong>{meetup.title}</strong>
                       <span>
-                        {meetup.subscriptions
-                          ? `${meetup.subscriptions.length} membros`
+                        {meetup.__meta__.subscriptions_count > 0
+                          ? `${meetup.__meta__.subscriptions_count} membros`
                           : 'Nenhum membro'}
                       </span>
                     </div>
@@ -120,16 +124,16 @@ class Search extends Component {
         <Box>
           <span>Recomendados</span>
           <MeetupsList>
-            {meetups
-              && meetups.data.map(meetup => (
+            {recommended
+              && recommended.map(meetup => (
                 <Meetup key={meetup.id}>
                   <img alt={meetup.title} src={`http://127.0.0.1:3333/files/${meetup.file_id}`} />
                   <div>
                     <div>
                       <strong>{meetup.title}</strong>
                       <span>
-                        {meetup.subscriptions
-                          ? `${meetup.subscriptions.length} membros`
+                        {meetup.__meta__.subscriptions_count > 0
+                          ? `${meetup.__meta__.subscriptions_count} membros`
                           : 'Nenhum membro'}
                       </span>
                     </div>
@@ -147,8 +151,9 @@ class Search extends Component {
 }
 
 const mapStateToProps = state => ({
-  meetups: state.meetups,
-  auth: state.auth,
+  subscriptions: state.meetups.subscriptions,
+  nexts: state.meetups.nexts,
+  recommended: state.meetups.recommended,
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators({ ...MeetupsActions, ...AuthActions }, dispatch);
